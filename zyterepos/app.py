7d57a4bbfb2249e448650363ab442ffb2dbfc0d6
eezipc <eezipc@gmail.com>
2021-10-22 15:04:41 +0100

    Show 10 results from git api.

--- zyterepos/app.py
+++ zyterepos/app.py
@@ -3,12 +3,12 @@ import requests
 import json
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def zyte():
-    zyterequest = requests.get('https://api.github.com/orgs/scrapinghub/repos')
-    zytedata = json.loads(zyterequest.content)
+    zyterequest = requests.get('https://api.github.com/orgs/scrapinghub/repos?per_page=10')
+    data = json.loads(zyterequest.content)
 
-    return render_template('index.html', data=zytedata)
+    return render_template('index.html', data=data)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
